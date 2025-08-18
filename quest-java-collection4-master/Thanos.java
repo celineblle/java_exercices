1ccdb1ad455a02c6381ac31d469d846a404e8d80
celineblle <celine.baille97@gmail.com>
2025-08-18 11:04:08 +0200

    resolution de l'exercice de map

--- quest-java-collection4-master/Thanos.java
+++ quest-java-collection4-master/Thanos.java
@@ -1,3 +1,7 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Thanos {
 
     public static void main(String[] args) {
@@ -17,14 +21,35 @@ public class Thanos {
         Flower rose = new Flower("Rose");
 
         // TODO 1 : Create a TreeMap where :
+        Comparator<Hero> comparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero hero, Hero compare) {
+                return hero.getName().compareTo(compare.getName());
+            }
+        };
+
+        Map<Hero, Flower> party = new TreeMap<>(comparator);
+
+        party.put(hulk, rose);
+        party.put(thor, rose);
+        party.put(scarletWitch, rose);
+        party.put(vision, tulip);
+        party.put(captainAmerica, lily);
+        party.put(doctorStrange, violet);
+        party.put(blackWidow, violet);
+
         // Hulk, Thor and Scarlet Witch have a Rose
         // Vision has a Tulip
         // Captain America has a Lily
         // Doctor Strange and Black Widow have a Violet
 
         // TODO 2 : Print if `begonia` is contained in the TreeMap
+        System.out.println(party.containsValue(begonia));
 
         // TODO 3 : For each hero, alphabetically, print the corresponding flower
-
+        for (Hero hero : party.keySet()) {
+            Flower flower = party.get(hero);
+            System.out.println(flower.getName());
+        }
     }
 }
